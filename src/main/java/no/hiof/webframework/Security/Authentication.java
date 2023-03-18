584e5c7cc15728152c893596a6f280a9436a86b5
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-03-18 14:12:25 +0100

    update scenario 3.5
    Opprettet interfaces for login-autentisering.

--- src/main/java/no/hiof/webframework/Security/Authentication.java
+++ src/main/java/no/hiof/webframework/Security/Authentication.java
@@ -1,73 +1,20 @@
 package no.hiof.webframework.Security;
-//Scenario 3.5, Metodene er kommentert for aa unngaa at programmet ikke kjoerer.
 
-public class Authentication {
+import no.hiof.webframework.Interface.Authenticator;
+/**
+ * The Authentication class handles user authentication.
+ */
+public class Authentication implements Authenticator {
     /**
-     * The Auth class handles user authentication.
+     * Authenticates a user with a given username and password.
+     *
+     * @param username the username to authenticate.
+     * @param password the password associated with the username.
+     * @return true if the user is authenticated, false otherwise.
      */
-    public class Auth {
-        /**
-         * Authenticates a user with a given username and password.
-         *
-         * @param username the username to authenticate.
-         * @param password the password associated with the username.
-         * @return true if the user is authenticated, false otherwise.
-         */
-
-       /* public boolean authenticate(String username, String password) {
-            // Valider brukernavn og passord
-        } */
-    }
-
-    /**
-     * The SMS class generates and sends verification codes via SMS.
-     */
-    public class SMS {
-        /**
-         * Generates a verification code and sends it to a given phone number.
-         *
-         * @param phoneNumber the phone number to send the verification code to.
-         * @return the generated verification code.
-         */
-       /* public String generateCode(String phoneNumber) {
-            // Generer og send kode via SMS
-        } */
-    }
-
-    /**
-     * The Login class handles user login using authentication and SMS verification.
-     */
-    public class Login {
-        private Auth auth;
-        private SMS sms;
-        /**
-         * Initializes a new instance of the Login class.
-         */
-        /* public Login() {
-            auth = new Auth();
-            sms = new SMS();
-        } */
-        /**
-         * Attempts to log in a user using authentication and SMS verification.
-         *
-         * @param username the username to authenticate.
-         * @param password the password associated with the username.
-         * @param phoneNumber the phone number associated with the user.
-         * @param code the verification code to check against the generated code.
-         * @return true if the user is successfully authenticated and verified, false otherwise.
-         */
-        /* public boolean login(String username, String password, String phoneNumber, String code) {
-            boolean authenticated = auth.authenticate(username, password);
-
-            if (authenticated) {
-                String generatedCode = sms.generateCode(phoneNumber);
-
-                if (code.equals(generatedCode)) {
-                    return true;
-                }
-            }
-
-            return false;
-        } */
+    @Override
+    public boolean authenticateLogIn(String username, String password) {
+        // Valider brukernavn og passord
+        return false;
     }
 }
---
+++ src/main/java/no/hiof/webframework/Security/LogInAuthentication.java
@@ -0,0 +1,44 @@
+package no.hiof.webframework.Security;
+
+import no.hiof.webframework.Interface.Authenticator;
+import no.hiof.webframework.Interface.SMSCodeGenerator;
+/**
+ * The LogInAuthentication class handles user login using authentication and SMS verification.
+ */
+public class LogInAuthentication {
+    private Authenticator auth;
+    private SMSCodeGenerator codeGenerator;
+    /**
+     * Initializes a new instance of the LogIn class with the specified Authenticator and SMSCodeGenerator objects.
+     *
+     * @param auth an object implementing the Authenticator interface.
+     * @param codeGenerator an object implementing the SMSCodeGenerator interface.
+     */
+    public LogInAuthentication(Authenticator auth, SMSCodeGenerator codeGenerator){
+        this.auth=auth;
+        this.codeGenerator=codeGenerator;
+    }
+
+    /**
+     * Attempts to log in a user using authentication and SMS verification.
+     *
+     * @param username the username to authenticate.
+     * @param password the password associated with the username.
+     * @param phoneNumber the phone number associated with the user.
+     * @param code the verification code to check against the generated code.
+     * @return true if the user is successfully authenticated and verified, false otherwise.
+     */
+    public boolean login(String username, String password, String phoneNumber, String code) {
+        boolean authenticated = auth.authenticateLogIn(username, password);
+
+        if (authenticated) {
+            String generatedCode = codeGenerator.generateCode(phoneNumber);
+
+            if (code.equals(generatedCode)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
