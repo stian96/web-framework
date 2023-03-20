2f1e088cbad802b9a4d7b0ea5dee17f9f0349f2f
a5h71 <ashtim@hiof.no>
2023-03-20 12:53:30 +0100

    commentary and constructor

--- src/main/java/no/hiof/webframework/Repository/EntityModel.java
+++ src/main/java/no/hiof/webframework/Repository/EntityModel.java
@@ -2,7 +2,17 @@ package no.hiof.webframework.Repository;
 
 import no.hiof.webframework.Interface.EntityModelBuilder;
 
+//Scenario 4.2
+/**
+ * Class for creating an entity model and adding to a database
+ */
 public class EntityModel implements EntityModelBuilder {
+    private final String tableName;
+
+
+    public EntityModel(String tableName) {
+        this.tableName = tableName;
+    }
 
     @Override
     public EntityModelBuilder setTableName(String tableName) {
