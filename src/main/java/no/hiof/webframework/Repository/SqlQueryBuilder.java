5bce874969420938c908e4c0318afbcaf5ea28c3
a5h71 <ashtim@hiof.no>
2023-03-20 15:04:39 +0100

    Implemented a method in SqlQueryBuilder

--- src/main/java/no/hiof/webframework/Repository/SqlQueryBuilder.java
+++ src/main/java/no/hiof/webframework/Repository/SqlQueryBuilder.java
@@ -1,6 +1,7 @@
 package no.hiof.webframework.Repository;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -11,6 +12,17 @@ public class SqlQueryBuilder {
     private String tableName;
     private List<String> columns = new ArrayList<>();
     private List<String> conditions = new ArrayList<>();
+
+    /**
+     * adds specified columns to the SELECT statement of the query
+     * @param columns
+     * @return columns
+     */
+    public String select(String columns){
+        //TODO
+        return columns;
+    }
+
 
 
 }
