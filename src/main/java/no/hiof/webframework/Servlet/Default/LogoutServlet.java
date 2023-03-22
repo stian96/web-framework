0049b326ff95cef8e07e35f2aca7bef3d138ca91
stian96 <stiankru@hiof.no>
2023-03-22 23:11:48 +0100

    Added javadoc comments to LogoutServlet class.

--- src/main/java/no/hiof/webframework/Servlet/Default/LogoutServlet.java
+++ src/main/java/no/hiof/webframework/Servlet/Default/LogoutServlet.java
@@ -4,12 +4,25 @@ import no.hiof.webframework.Frontend.HtmlPages;
 
 import java.io.InputStream;
 
+/**
+ * A servlet class that handles the logout functionality of the web application.
+ * Extends the AbstractServlet class and implements the getPageContent method to
+ * retrieve the content of the logout page.
+ */
 public class LogoutServlet extends AbstractServlet {
-
+    /**
+     * Constructs a LogoutServlet object with the specified HTML page and title.
+     * @param page the HTML page that contains the logout content
+     * @param title the title of the logout page
+     */
     public LogoutServlet(HtmlPages page, String title) {
         super(page, title);
     }
 
+    /**
+     * Retrieves the content of the logout page.
+     * @return an InputStream containing the content of the logout page
+     */
     @Override
     protected InputStream getPageContent() {
         return getPage().getLogoutPage();
