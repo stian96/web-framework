74b17985590a6ec5c8b8f5288c4eb54d8b13479a
stian96 <stiankru@hiof.no>
2023-03-22 22:20:04 +0100

    Added javadoc comments to AbstractHtmlFactory interface.

--- src/main/java/no/hiof/webframework/Interface/Builders/AbstractHtmlFactory.java
+++ src/main/java/no/hiof/webframework/Interface/Builders/AbstractHtmlFactory.java
@@ -1,12 +1,26 @@
 package no.hiof.webframework.Interface.Builders;
 
 /**
- * Abstract html-factory interface which can deliver
- * ready-made standard web-application pages.
- * @param <T> Generic parameter.
+ * This is an abstract interface for an HTML factory which creates pre-built standard web application pages.
+ * It defines three methods to create a login page, home page, and logout page.
+ * @param <T> The generic type of the pages that are created.
  */
 public interface AbstractHtmlFactory<T> {
+    /**
+     * Creates a pre-built login page which is fully designed and styled.
+     * @return The pre-built login page as an object of type T.
+     */
     T createLoginPage();
+
+    /**
+     * Creates a pre-built home page which is fully designed and styled.
+     * @return The pre-built home page as an object of type T.
+     */
     T createHomePage();
+
+    /**
+     * Creates a pre-built logout page which is fully designed and styled.
+     * @return The pre-built logout page as an object of type T.
+     */
     T createLogoutPage();
 }
