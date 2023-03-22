9717c4869dba41950dd25572e6c19978e8524b11
stian96 <stiankru@hiof.no>
2023-03-22 22:27:17 +0100

    Added javadoc comments to IHtmlPage interface.

--- src/main/java/no/hiof/webframework/Interface/IHtmlPage.java
+++ src/main/java/no/hiof/webframework/Interface/IHtmlPage.java
@@ -2,8 +2,26 @@ package no.hiof.webframework.Interface;
 
 import java.io.InputStream;
 
+/**
+ * This is an interface for pre-built HTML pages. It defines methods to retrieve input streams for the login page,
+ * home page, and logout page.
+ */
 public interface IHtmlPage {
+    /**
+     * Returns an input stream for the pre-built login page.
+     * @return An input stream for the login page.
+     */
     InputStream getLoginPage();
+
+    /**
+     * Returns an input stream for the pre-built home page.
+     * @return An input stream for the home page.
+     */
     InputStream getHomePage();
+
+    /**
+     * Returns an input stream for the pre-built logout page.
+     * @return An input stream for the logout page.
+     */
     InputStream getLogoutPage();
 }
