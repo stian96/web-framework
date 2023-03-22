82807d0825fb3aa7f882c6b238d95471b50ac810
stian96 <stiankru@hiof.no>
2023-03-22 23:10:21 +0100

    Added javadoc comments to LoginServlet class.

--- src/main/java/no/hiof/webframework/Servlet/Default/LoginServlet.java
+++ src/main/java/no/hiof/webframework/Servlet/Default/LoginServlet.java
@@ -5,14 +5,34 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * The LoginServlet class is a subclass of the AbstractServlet class,representing the login page of a web application.
+ * It allows users to log in by entering their username and password.
+ */
 public class LoginServlet extends AbstractServlet {
-
+    /**
+     * Constructs a LoginServlet object with the specified HtmlPages and title.
+     * @param page an instance of HtmlPages that contains information about the login page.
+     * @param title the title of the login page.
+     */
     public LoginServlet(HtmlPages page, String title) {
         super(page, title);
     }
 
+    /**
+     * Constructs a LoginServlet object.
+     */
     public LoginServlet() {}
 
+    /**
+     * Handles POST requests sent to the login page.
+     * Validates the user's credentials by checking them against a database.
+     * If the user exists in the database, redirects them to the home page.
+     * If not, redirects them back to the login page.
+     * @param request the HttpServletRequest object containing the request made to the servlet
+     * @param response the HttpServletResponse object containing the response that will be sent back to the client
+     * @throws IOException if an input or output exception occurred while handling the request
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
@@ -28,6 +48,10 @@ public class LoginServlet extends AbstractServlet {
         }
     }
 
+    /**
+     * Returns the InputStream of the login page content.
+     * @return the InputStream of the login page content
+     */
     @Override
     protected InputStream getPageContent() {
         return getPage().getLoginPage();
