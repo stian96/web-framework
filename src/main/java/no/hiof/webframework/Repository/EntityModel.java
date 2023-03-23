1acbe8591a8b7814b578cbba5e87b8beec9e54d5
a5h71 <ashtim@hiof.no>
2023-03-23 10:38:31 +0100

    Fixed the javadoc comments and added the Field class a nested class for better control

--- src/main/java/no/hiof/webframework/Repository/EntityModel.java
+++ src/main/java/no/hiof/webframework/Repository/EntityModel.java
@@ -25,23 +25,24 @@ public class EntityModel implements EntityModelBuilder {
 
     /**
      * Method for setting the table name in the schema
-     * @param tableName
-     * @return an instance of EntityModelBuilder
+     * @param tableName the name of the table for the entity model
+     * @return an instance of EntityModelBuilder with updated table name
      */
 
     @Override
     public EntityModelBuilder setTableName(String tableName) {
-
+        this.tableName = tableName;
         return this;
     }
 
     /**
-     * Method that adds a new field to the entity
-     * @param name of column
-     * @param type datatype of the column
-     * @param nullable
-     * @return EntityModelBuilder instance
-     */
+
+    * Overrides the superclass method to add a new field to the entity model builder.
+    * @param name the name of the field
+    * @param type the data type of the field
+    * @param nullable indicates if the field can be null
+    */
+
     @Override
     public EntityModelBuilder addField(String name, String type, boolean nullable) {
         fields.add(new Field(name,type,nullable));
@@ -64,6 +65,28 @@ public class EntityModel implements EntityModelBuilder {
      */
     public void generateSchema(){
         //TODO
+    }
+
+    /**
+     * The Field class represents a field in an entity model, consisting of a name, data type, and nullability.
+     */
+    public static class Field {
+
+        private final String name;
+        private final String type;
+        private final boolean nullable;
+
+        /**
+         * Constructs a new Field object
+         * @param name name the name of the field
+         * @param type type the data type of the field
+         * @param nullable nullable indicates if the field can be null
+         */
+        public Field(String name, String type, boolean nullable) {
+            this.name = name;
+            this.type = type;
+            this.nullable = nullable;
+        }
     }
 
 }
