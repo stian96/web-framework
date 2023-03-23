5d3d2868e8af48892d68c3a06ccf2c3c76c42ad4
a5h71 <ashtim@hiof.no>
2023-03-23 10:37:50 +0100

    Fixed the javadoc comments

--- src/main/java/no/hiof/webframework/Interface/EntityModelBuilder.java
+++ src/main/java/no/hiof/webframework/Interface/EntityModelBuilder.java
@@ -3,33 +3,33 @@ package no.hiof.webframework.Interface;
 import no.hiof.webframework.Repository.EntityModel;
 
 /**
- * Still trying out ideas
+
+ * This interface defines a builder for creating an EntityModel.
  */
 public interface EntityModelBuilder {
-
     /**
-     * Method that sets the table name takes
-     * @param tableName
-     * @return table
+
+     * Sets the table name for the entity model.
+     * @param tableName the name of the table for the entity model
+     * @return the entity model builder with the updated table name
      */
     EntityModelBuilder setTableName(String tableName);
-
     /**
-     * Method that takes the following parameters
-     * @param name
-     * @param type
-     * @param nullable
-     * @return //TODO
+
+     * Adds a new field to the entity model.
+     * @param name the name of the field
+     * @param type the data type of the field
+     * @param nullable indicates if the field can be null
+     * @return the entity model builder with the new field added
+
      */
     EntityModelBuilder addField(String name, String type, boolean nullable);
+    /**
 
-    /**
-     * Method that uses the above methods to return
-     * @return EntityModel
+     * Builds the entity model based on the previously added fields and table name.
+     * @return the built entity model
      */
+
     EntityModel build();
-    /**
-     * Not sure about the above class or method
-     * //TODO
-     */
+
 }
