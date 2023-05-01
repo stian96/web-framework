549cfa4a0ab855c5ca71ee9035aa4529159fbbbe
stian96 <stiankru@hiof.no>
2023-05-01 22:37:55 +0200

    added a main file.

--- src/main/java/no/hiof/webframework/Main.java
+++ src/main/java/no/hiof/webframework/Main.java
@@ -1,7 +1,13 @@
 package no.hiof.webframework;
 
+import no.hiof.webframework.application.App;
+
 public class Main {
     public static void main(String[] args) {
+
+        App app = App.create();
+        app.setApplicationTitle("h");
+        app.run();
 
     }
 }
