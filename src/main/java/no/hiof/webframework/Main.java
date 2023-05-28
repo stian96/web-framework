7d2cdc6f19dcfa2f7da20b0f0b4728390364521c
stian96 <stiankru@hiof.no>
2023-05-28 13:23:51 +0200

    small change

--- src/main/java/no/hiof/webframework/Main.java
+++ src/main/java/no/hiof/webframework/Main.java
@@ -1,8 +1,12 @@
 package no.hiof.webframework;
 
 
+import no.hiof.webframework.servers.ConfigureServer;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+
 
 
 
