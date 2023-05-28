c4f6c55f9c9e4ab5179b2f31e20a0954c9710841
stian96 <stiankru@hiof.no>
2023-05-28 13:16:06 +0200

    removed stuff from main

--- src/main/java/no/hiof/webframework/Main.java
+++ src/main/java/no/hiof/webframework/Main.java
@@ -1,13 +1,9 @@
 package no.hiof.webframework;
-import no.hiof.webframework.application.App;
-import no.hiof.webframework.controllers.TestController;
-import no.hiof.webframework.servers.ConfigureServer;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        App app = App.create();
 
 
     }
