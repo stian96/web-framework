52e6d810ff97457176602b482c5dfb1a8f2d195c
stian96 <stiankru@hiof.no>
2023-05-28 13:22:15 +0200

    deleted manifest file.

--- src/main/java/no/hiof/webframework/Main.java
+++ src/main/java/no/hiof/webframework/Main.java
@@ -3,19 +3,11 @@ import no.hiof.webframework.application.App;
 import no.hiof.webframework.application.Chatroom;
 import no.hiof.webframework.application.enums.ChatMethod;
 import no.hiof.webframework.application.enums.Options;
+import no.hiof.webframework.servers.ConfigureServer;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        App app = App.create();
-        Chatroom.setChatMethod(ChatMethod.PRIVATE);
-        Chatroom.addDeleteMessagesButton(Options.YES);
-        Chatroom.setTitle("chat club!");
-        Chatroom.addMessageTimeStamp(true);
-
-        app.addChatRoom(Chatroom.create());
-        app.run();
 
     }
 }
