286999bde4acc658e0f0e11bd99bf185d6906b11
stian96 <stiankru@hiof.no>
2023-05-28 13:39:33 +0200

    merged branch stian-test into main

--- src/main/java/no/hiof/webframework/Main.java
+++ src/main/java/no/hiof/webframework/Main.java
@@ -1,13 +1,8 @@
 package no.hiof.webframework;
 
 
-import no.hiof.webframework.servers.ConfigureServer;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-
-
-
 
 
     }
