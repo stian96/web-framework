fb0088f61ed078be3a1d9dc5893a4fd405c5c369
stian96 <stiankru@hiof.no>
2023-05-28 13:34:35 +0200

    small change

--- src/main/java/no/hiof/webframework/Main.java
+++ src/main/java/no/hiof/webframework/Main.java
@@ -1,13 +1,11 @@
 package no.hiof.webframework;
-import no.hiof.webframework.application.App;
-import no.hiof.webframework.application.Chatroom;
-import no.hiof.webframework.application.enums.ChatMethod;
-import no.hiof.webframework.application.enums.Options;
-import no.hiof.webframework.servers.ConfigureServer;
+
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
+
 
     }
 }
