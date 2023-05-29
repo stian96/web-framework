19f1e530dd2e49724bd7c3ea6cd651493f541aba
a5h71 <ashtim@hiof.no>
2023-05-29 15:36:19 +0200

    Added javadoc comment to RepositoryConnectionFactory.java

--- src/main/java/no/hiof/webframework/interfaces/RepositoryConnectionFactory.java
+++ src/main/java/no/hiof/webframework/interfaces/RepositoryConnectionFactory.java
@@ -2,6 +2,15 @@ package no.hiof.webframework.interfaces;
 
 import no.hiof.webframework.repository.RepositoryConnection;
 
+/**
+ * The {@code RepositoryConnectionFactory} interface defines a contract for creating instances of {@link RepositoryConnection}.
+ * It serves as a factory interface for producing {@link RepositoryConnection} objects.
+ */
 public interface RepositoryConnectionFactory {
+    /**
+     * Creates a new instance of {@link RepositoryConnection}.
+     * @return a {@link RepositoryConnection} object
+     *
+     */
     RepositoryConnection createConnection();
 }
